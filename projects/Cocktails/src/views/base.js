c3ef7d3d15c5c37ce73861e797d8c7b959a86795
Pettergov <44157268+Pettergov@users.noreply.github.com>
2019-08-20 13:05:53 +0200

    1

---
+++ projects/Cocktails/src/views/base.js
@@ -0,0 +1,10 @@
+export const elementsDOM = {
+  search: document.querySelector('.search'),
+  searchIngredient: document.querySelector('#searchIngredient'),
+  btnGetIngredient: document.querySelector('.btnGetIngredient'),
+  searchName: document.querySelector('#searchName'),
+  btnGetName: document.querySelector('.btnGetName'),
+  searchResultList: document.querySelector('.result-list'),
+  cardDispaly: document.querySelector('.card-display'),
+  pages: document.querySelector('.pages')
+}
