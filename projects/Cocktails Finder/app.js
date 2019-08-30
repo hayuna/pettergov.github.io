c314cfed61de4b28e7d5701e51b83c783c0aa583
mras2303 <marek.kowalonek@gmail.com>
2019-08-31 01:07:09 +0200

    mras2303 update

--- projects/Cocktails Finder/app.js
+++ projects/Cocktails Finder/app.js
@@ -1,111 +1,71 @@
-
-// Init UI
-const ui = new UI;
-const error = new Error;
-// Search by ingredient input and button
+const ui = new UI();
 const searchIngredient = document.querySelector('#searchIngredient');
-const btnGetIngredient = document.querySelector('.btnGetIngredient')
+const btnGetIngredient = document.querySelector('.btnGetIngredient');
 
-// Search by ingredient event listener
 btnGetIngredient.addEventListener('click', () => {
-  // Get input text
-  let inputText = searchIngredient.value.toUpperCase();
-  //console.log(inputText);    
-  
-  if(inputText == '') {
-    ui.showAlert('Please fill in ingredient field');
-    
-  } else{
-    const xhr = new XMLHttpRequest();
+  const inputText = searchIngredient.value.toUpperCase();
 
-    xhr.open('GET', `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${inputText}`, true);
-
-    console.log(xhr);
-    
-    xhr.onload = function() {
-        if(this.responseText.slice(2,8) == 'drinks') {
-          console.log(this.responseText.slice(2,8) == 'drinks');
-          const response = JSON.parse(this.responseText);
-          
-          let drinksArray = Array.from(response.drinks);
-          console.log(drinksArray);
-          ui.showProfile(drinksArray);
-          searchIngredient.value = '';
-
-        } else {
-          ui.showAlert(`Not found ${inputText} ingredient's name`)
-          searchIngredient.value = '';
-          console.log(xhr);
-        }        
-    }
-    xhr.send();
+  if(inputText) {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${inputText}`)
+      .then(response => {
+        if(!response) throw new Error()
+        return response
+      })
+      .then(response => response.json())
+      .then(response => {
+        if(response.drinks) {
+          ui.showProfile(response.drinks);
+        }   
+      })
+      .catch(() => {
+        ui.showAlert(`Not found ${inputText} ingredient's name`)
+      })
+      .finally(() => {
+        searchIngredient.value = '';
+      });
+  } else {
+    ui.showAlert('Please fill in ingredient field');  
   }
 })
 
-
-
-// Search by name input and button
-  const searchName = document.querySelector('#searchName');
-  const btnGetName = document.querySelector('.btnGetName');
-  // Search input event listener
-  btnGetName.addEventListener('click', () => {
-    // Get input text
-    let inputText = searchName.value.toUpperCase();
-    //console.log(inputText);
-
-    if(inputText == '') {
-      ui.showAlert('Please fill in the field');
-      
-    } else{
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputText}`, true);
-    
-      console.log(xhr);
-      xhr.onload = function() {
-          
-        const response = JSON.parse(this.responseText);
-          if(response.drinks === null){
-            console.log(this.response.drinks);
-            ui.showAlert(`Not found ${inputText} cocktail's name`)
-            searchName.value = ''
-            console.log(xhr);
-          } else {
-            console.log(response);
-            let drinksArray = Array.from(response.drinks);
-            console.log(drinksArray);
-            ui.showNameProfile(drinksArray);
-            searchName.value = '';
-          }        
-        }
-      xhr.send(); 
-    }
+const searchName = document.querySelector('#searchName');
+const btnGetName = document.querySelector('.btnGetName');
+btnGetName.addEventListener('click', () => {
+  const inputText = searchName.value.toUpperCase();
+  if(inputText.length) {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputText}`)
+      .then(response => response.json())
+      .then(response => {
+        if(response.drinks){
+          ui.showNameProfile(response.drinks);
+        }     
+      })
+      .catch(() => {
+        ui.showAlert(`Not found ${inputText} cocktail's name`);
+      })
+      .finally(() => {
+        searchName.value = '';
+      })
+  } else{
+    ui.showAlert('Please fill in the field');
+  }
 })
 
-
-// Click and show description of cocktail
 document.querySelector('#profile').addEventListener('click', showCocktail);
 
 function showCocktail(e) {
   if(e.target.classList.contains('btn-description')) {
-    
-    let cocktailName = e.target.parentElement.childNodes[1].textContent;
-    console.log(cocktailName);
-    console.log(e.target.parentElement.childNodes[1].textContent);
-
-    const xhr = new XMLHttpRequest();
-      xhr.open('GET', `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`, true);
-    
-      console.log(xhr);
-      xhr.onload = function() {
-        const response = JSON.parse(this.responseText);
-        let drinksArray = Array.from(response.drinks);
-        console.log(drinksArray);
-        ui.showNameProfile(drinksArray);
+    const cocktailName = e.target.parentElement.childNodes[1].textContent;
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
+      .then(response => response.json())
+      .then(response => {
+        ui.showNameProfile(response.drinks);
+      })
+      .catch(e => {
+        ui.showAlert(e);
+      })
+      .finally(() => {
         searchName.value = '';
-      }
-      xhr.send();
+      })
   }
 }
-
-
-   
